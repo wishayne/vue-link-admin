68f4b6e1ab458adf17434419f0ae934f2cf830d9
YuxinWang <1325943548@qq.com>
2019-11-11 09:08:05 +0800

    加入服务管理部分

---
+++ src/router/modules/service-mange.js
@@ -0,0 +1,41 @@
+/** When your routing table is too long, you can split it into small modules**/
+
+import Layout from '@/layout'
+
+const serviceManageRouter = {
+  path: '/service-mange',
+  component: Layout,
+  name: 'service-mange',
+  meta: {
+    title: '服务管理',
+    icon: 'lock'
+  },
+  children: [
+    {
+      path: '/service-mange/add-service/',
+      component: () => import('@/views/service-mange/add-service/'),
+      name: 'add-service',
+      meta: {
+        title: '服务注册', noCache: true
+      }
+    },
+    {
+      path: '/service-mange/list-service/',
+      component: () => import('@/views/service-mange/list-service/'),
+      name: 'list-service',
+      meta: {
+        title: '搜索服务', noCache: true
+      }
+    },
+    {
+      path: '/service-mange/service-category/',
+      component: () => import('@/views/service-mange/service-category/'),
+      name: 'service-category',
+      meta: {
+        title: '服务目录管理', noCache: true
+      }
+    }
+  ]
+}
+
+export default serviceManageRouter
